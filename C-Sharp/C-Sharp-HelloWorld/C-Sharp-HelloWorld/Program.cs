e21b695e95e6f808cff217f1c80d35811e4f572a
Christoph Schmitz <cschmitz@intra.geva.de>
2019-05-10 11:18:02 +0200

    cSharp Project

---
+++ C-Sharp/C-Sharp-HelloWorld/C-Sharp-HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C_Sharp_HelloWorld
+{
+    class HelloWOrld
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            for(a=0;  a< 10; a++)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
